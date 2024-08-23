462fa076d24764b2c3298d744f1ad0506120aa35
Matheus <matheus.mleall@gmail.com>
2024-08-23 10:18:27 -0300

    feat: added an image to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,23 +4,25 @@ import { main_font_mono, main_font_sans, second_font } from "./functions/fonts";
 import StyledComponentsRegistry from "./lib/registry";
 
 export const metadata: Metadata = {
-	title: "Mr. Chef",
-	description: "Gerencie seu restaurante e alavanque suas vendas",
+  title: "Mr. Chefe",
+  description: "Gerencie seu restaurante e alavanque suas vendas",
+  openGraph: {
+    images: ["/images/coffe-shop-login-page.webp"],
+  },
 };
-
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html
-			lang='pt-BR'
-			className={`${main_font_mono.variable} ${main_font_sans.variable} ${second_font.variable}`}
-		>
-			<body>
-				<StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-			</body>
-		</html>
-	);
+  return (
+    <html
+      lang="pt-BR"
+      className={`${main_font_mono.variable} ${main_font_sans.variable} ${second_font.variable}`}
+    >
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  );
 }
