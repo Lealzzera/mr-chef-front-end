9242ed8058a8dcb40d24a54efdda3b6377549390
Matheus <matheus.mleall@gmail.com>
2024-09-15 13:09:20 -0300

    replaced refresh api to get env from env file ts

--- src/app/api/refresh/route.ts
+++ src/app/api/refresh/route.ts
@@ -1,10 +1,11 @@
+import { env } from "@/env";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST() {
   try {
     const authorization = headers().get("refresh_token");
-    const res = await fetch(`${process.env.MR_CHEFE_URL}/auth/refresh`, {
+    const res = await fetch(`${env.MR_CHEFE_URL}/auth/refresh`, {
       method: "POST",
       headers: { "Content-type": "application/json" },
     }).then((result) => {
