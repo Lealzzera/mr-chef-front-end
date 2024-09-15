8c8f5e7d9b7c174b537ffe8c7e2355e9f80fefbb
Matheus <matheus.mleall@gmail.com>
2024-09-15 02:35:14 -0300

    fixed api auth route

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -2,36 +2,36 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  try {
-    const { email, password } = await req.json();
-    const { accessToken } = await fetch(
-      `${process.env.MR_CHEFE_URL}/auth/sessions`,
-      {
-        method: "POST",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify({ email, password }),
-        credentials: "include",
-      }
-    ).then((res) => {
-      console.log(res);
-      const refreshToken = res.headers.get("set-cookie");
-      if (refreshToken) {
-        cookies().set({
-          name: "refresh_token",
-          value: refreshToken.split("=")[1].split(";")[0],
-          maxAge: 60 * 1000 * 60 * 24 * 7,
-          httpOnly: true,
-          sameSite: "strict",
-          path: "/",
-        });
-      }
-      return res.json();
-    });
+  const { email, password } = await req.json();
+  const response = await fetch(`${process.env.MR_CHEFE_URL}/auth/sessions`, {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
 
-    cookies().set("access_token", accessToken);
-
-    return NextResponse.json(accessToken, { status: 200 });
-  } catch (error: any) {
-    return NextResponse.json({ message: error.message }, { status: 401 });
+  if (!response.ok) {
+    const { message, error, statusCode } = await response.json();
+    return NextResponse.json({ message, error }, { status: statusCode });
   }
+
+  const refreshTokenCookie = response.headers.get("set-cookie");
+
+  if (refreshTokenCookie) {
+    const sevenDaysAge = 60 * 1000 * 60 * 24 * 7;
+    const refreshTokenCookieValue = refreshTokenCookie
+      ?.split("=")[1]
+      .split(";")[0];
+    cookies().set("refresh_token", refreshTokenCookieValue, {
+      maxAge: sevenDaysAge,
+      httpOnly: true,
+      sameSite: "strict",
+      path: "/",
+    });
+  }
+
+  const accessToken = await response.json();
+  return NextResponse.json(accessToken);
 }
