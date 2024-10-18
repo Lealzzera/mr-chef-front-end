9e355bc137de54a5cc3c192747609344a5552170
Matheus Leal <matheus.mleall@gmail.com>
2024-10-18 13:42:43 -0300

    index header style compoennt

--- src/components/IndexPage/styles.ts
+++ src/components/IndexPage/styles.ts
@@ -2,36 +2,6 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div``;
 
-export const HeaderPage = styled.header`
-  display: flex;
-  margin: 0 auto;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1rem 2rem;
-  @media (min-width: 1200px) {
-    padding: 1.875rem 9.375rem;
-  }
-`;
-
-export const LogoContainer = styled.div`
-  display: flex;
-  align-items: end;
-  gap: 0.5rem;
-`;
-
-export const LogoText = styled.p`
-  font-size: 1rem;
-  color: var(--green-300);
-`;
-
-export const LoginButtonContainer = styled.div`
-  & .MuiButtonBase-root.MuiButton-root {
-    text-transform: capitalize;
-    font-weight: 400;
-    height: 2rem;
-  }
-`;
-
 export const CreateAccountSection = styled.section`
   padding: 0 2rem;
   margin-top: 1.5rem;
