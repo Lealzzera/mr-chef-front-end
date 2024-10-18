09b3126d7329fc919e5b721b64ffac1a6f85d8b1
Matheus Leal <matheus.mleall@gmail.com>
2024-10-17 22:21:46 -0300

    Added new card icons

---
+++ src/icons/ShoppingBagIcon.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const ShoppingBagIcon = () => {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10 3.33337L5 10V33.3334C5 34.2174 5.35119 35.0653 5.97631 35.6904C6.60143 36.3155 7.44928 36.6667 8.33333 36.6667H31.6667C32.5507 36.6667 33.3986 36.3155 34.0237 35.6904C34.6488 35.0653 35 34.2174 35 33.3334V10L30 3.33337H10Z"
+        stroke="#00691E"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5 10H35"
+        stroke="#00691E"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M26.6666 16.6666C26.6666 18.4347 25.9642 20.1304 24.714 21.3807C23.4637 22.6309 21.768 23.3333 19.9999 23.3333C18.2318 23.3333 16.5361 22.6309 15.2859 21.3807C14.0356 20.1304 13.3333 18.4347 13.3333 16.6666"
+        stroke="#00691E"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ShoppingBagIcon;
